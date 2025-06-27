b53cffa89f63fa2afba64c9eba0974d11e673c45
Victor <victor.lenain26@gmail.com>
2025-06-27 17:31:11 +0200

    add test

--- prisma/migrations/20250627121117_add_theme_to_tenant/migration.sql
+++ prisma/migrations/20250627121117_add_theme_to_tenant/migration.sql
@@ -1,14 +1,21 @@
 /*
-  Warnings:
+  Warnings :
 
-  - You are about to drop the column `bgColor` on the `Tenant` table. All the data in the column will be lost.
-  - You are about to drop the column `primaryColor` on the `Tenant` table. All the data in the column will be lost.
-
+  - You are about to drop the column `bgColor` on the `Tenant` table.
+  - You are about to drop the column `primaryColor` on the `Tenant` table.
+    All the data in the column will be lost.
 */
--- AlterTable
-ALTER TABLE "Invite" ALTER COLUMN "expiresAt" SET DEFAULT now() + interval '30 days';
 
--- AlterTable
+-- Tenant : on sécurise les DROP pour qu’ils passent même si les colonnes
+-- n’existent pas encore (base fraîche).
+-- Invite : on laisse la modification du default inchangée.
+
+-- AlterTable Invite
+ALTER TABLE "Invite"
+  ALTER COLUMN "expiresAt"
+  SET DEFAULT now() + interval '30 days';
+
+-- AlterTable Tenant
 ALTER TABLE "Tenant"
   DROP COLUMN IF EXISTS "bgColor",
   DROP COLUMN IF EXISTS "primaryColor",
